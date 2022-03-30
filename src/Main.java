301b7b5fcd1d3ff89a30db8be1c484223d20db18
alexgim <alexgim961101@gmail.com>
2022-03-30 16:27:57 +0900

    Add Evie to Leopards

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int solution(int[] a, int[] b){
+    public static int solution(int[] nums){
         int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length-2; i++){
+            for(int j = i+1; j < nums.length-1; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    list.add(nums[i] + nums[j] + nums[k]);
+                }
+            }
+        }
 
-        for(int i = 0; i < a.length; i++){
-            answer += (a[i] * b[i]);
+        for(Integer x : list){
+            int count = 2;
+            answer++;
+
+            while(count < x){
+                if(x % count == 0){
+                    answer--;
+                    break;
+                }
+                count++;
+            }
         }
 
         return answer;
